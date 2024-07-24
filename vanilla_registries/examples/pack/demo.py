d2b2f5de3fae51c5bd60a02f36ca62a69dfa3261
edayot <pro.e.dayot@gmail.com>
2024-07-24 17:07:45 +0200

    feat: all supports

--- vanilla_registries/examples/pack/demo.py
+++ vanilla_registries/examples/pack/demo.py
@@ -1,10 +1,7 @@
-
 from beet import Context
 from vanilla_registries import GeneratedData
 
 
 def beet_default(ctx: Context):
     a = ctx.inject(GeneratedData)
-    print(a.registries["block"])
-    print(a.packets["play"])
-
+    print(a.blocks["barrel"])
--- vanilla_registries/main.py
+++ vanilla_registries/main.py
@@ -1,30 +1,63 @@
-
-from beet import Context, Container
+from beet import Context, Container, JsonFile, Cache
 from beet.contrib.vanilla import Vanilla
 import subprocess
 import os
 import json
 from pydantic import BaseModel
-from typing import NotRequired
+from typing import Any, TypeVar, Optional
+from pathlib import Path
+from dataclasses import dataclass
+
+V = TypeVar("V")
 
 
-
-class Registries(Container[str, set[str]]):
-    def __init__(self, data: dict):
-        super().__init__()
-        for key, value in data.items():
-            self[key] = set(value["entries"].keys())
-        
+class MinecraftContainer(Container[str, V]):
     def normalize_key(self, key: str) -> str:
         return key if ":" in key else f"minecraft:{key}"
 
+
+class BlockState(BaseModel):
+    default: Optional[bool] = None
+    id: int
+    properties: Optional[dict[str, str]] = None
+
+
+class Block(BaseModel):
+    definition: dict[str, Any]
+    properties: Optional[dict[str, list[str]]] = None
+    states: list[BlockState]
+
+
+class Blocks(MinecraftContainer[Block]):
+    def __init__(self, data: dict):
+        super().__init__()
+        for key, value in data.items():
+            try:
+                self[key] = Block(**value)
+            except Exception as e:
+                print(key, value)
+
+
+class Components(MinecraftContainer[Any]):
+    def __init__(self, data: dict):
+        super().__init__()
+        for key, value in data.items():
+            self[key] = value
+
+
+class Items(MinecraftContainer[Components]):
+    def __init__(self, data: dict):
+        super().__init__()
+        for key, value in data.items():
+            self[key] = Components(value.get("components", {}))
 
 
 class Packet(BaseModel):
     clientbound: set[str]
     serverbound: set[str]
 
-class Packets(Container[str, Packet]):
+
+class Packets(MinecraftContainer[Packet]):
     def __init__(self, data: dict):
         super().__init__()
         for key, value in data.items():
@@ -34,27 +67,103 @@ class Packets(Container[str, Packet]):
             )
 
 
-class GeneratedData:
+class Registries(MinecraftContainer[set[str]]):
+    def __init__(self, data: dict):
+        super().__init__()
+        for key, value in data.items():
+            self[key] = set(value["entries"].keys())
+
+
+@dataclass
+class Files:
     registries: Registries
     packets: Packets
+    items: Items
+    commands: JsonFile
+    blocks: Blocks
+
+
+
+class GeneratedData:
+    ctx: Context
+    cache: Cache
+    save_path: Path
+    reports: Path
+    minecraft_version: str
+
+    files: Files
 
     def __init__(self, ctx: Context):
-        cache = ctx.cache["vanilla_registries"]
-        save_path = cache.directory / "generated"
-        reports = save_path / "generated" / "reports"
+        self.ctx = ctx
+        self.vanilla = ctx.inject(Vanilla)
+        self.cache = ctx.cache["vanilla_registries"]
+        self.save_path = self.cache.directory / "generated"
+        self.reports = self.save_path / "generated" / "reports"
+        self.minecraft_version = self.vanilla.minecraft_version
+    
+    def regen(self):
+        release = self.vanilla.releases[self.minecraft_version]
+        jar = release.cache.download(
+            release.info.data["downloads"]["server"]["url"]
+        )
+        os.makedirs(self.save_path, exist_ok=True)
+        args = [
+            "java",
+            "-DbundlerMainClass=net.minecraft.data.Main",
+            "-jar",
+            jar,
+            "--reports",
+        ]
 
-        if cache.json.get("generated", True):
-            vanilla = ctx.inject(Vanilla)
-            release = vanilla.releases[vanilla.minecraft_version]
-            jar = release.cache.download(release.info.data["downloads"]["server"]["url"])
-            os.makedirs(save_path, exist_ok=True)
-            args = ["java", "-DbundlerMainClass=net.minecraft.data.Main", "-jar", jar, "--reports"]
+        subprocess.run(args, cwd=self.save_path, check=True)
 
-            subprocess.run(args, cwd=save_path, check=True)
-        cache.json["generated"] = False
+        with open(self.reports / "registries.json", "r") as f:
+            registries = Registries(json.load(f))
+        with open(self.reports / "packets.json") as f:
+            packets = Packets(json.load(f))
+        with open(self.reports / "items.json") as f:
+            items = Items(json.load(f))
+        commands = JsonFile(source_path=self.reports / "commands.json")
+        with open(self.reports / "blocks.json") as f:
+            blocks = Blocks(json.load(f))
 
-        with open(reports / "registries.json") as f:
-            self.registries = Registries(json.load(f))
-        with open(reports / "packets.json") as f:
-            self.packets = Packets(json.load(f))
+        self.files = Files(
+            registries=registries,
+            packets=packets,
+            items=items,
+            commands=commands,
+            blocks=blocks,
+        )
+    
+    def ensure(self):
+        if self.cache.json.get("minecraft_version") != self.minecraft_version:
+            self.regen()
+            self.cache.json["minecraft_version"] = self.minecraft_version
+        
+    @property
+    def registries(self) -> Registries:
+        self.ensure()
+        return self.files.registries
+    
+    @property
+    def packets(self) -> Packets:
+        self.ensure()
+        return self.files.packets
+    
+    @property
+    def items(self) -> Items:
+        self.ensure()
+        return self.files.items
+    
+    @property
+    def commands(self) -> JsonFile:
+        self.ensure()
+        return self.files.commands
+    
+    @property
+    def blocks(self) -> Blocks:
+        self.ensure()
+        return self.files.blocks
 
+        
+        
